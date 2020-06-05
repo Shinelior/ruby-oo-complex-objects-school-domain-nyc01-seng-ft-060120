3321792c78fc22b3de406542b27c2bba5e4b4611
lior Shine <shinelior@gmail.com>
2020-06-05 04:13:24 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,37 @@
 # code here!
+require "pry"
+class School
+  @@all = []
+   attr_accessor :student ,:grade
+  def initialize(school_name)
+    #@roster = {}
+   
+    @school_name = school_name
+    @@all << self
+   end
+ 
+  def roster
+    @roster = {}
+  end
+  
+  def add_student(new_student,new_grade)
+    self.student = new_student
+    self.grade = new_grade
+   
+     # @roster = {}
+     binding.pry
+     
+    @roster[new_grade] = []
+   
+    @roste[new_grade] << new_student
+    binding.pry
+end
+end
+# def grade(grade,student)
+#   if grade ==
+  
+# end
+ school = School.new("Bayside High School")
+# school.roster
+# school.add_student("Zach Morris", 9)
+# school.roster
